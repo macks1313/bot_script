42ee802041f0c294867e0ef9ba3280e462657f5b
macks1313 <maximefeger93@gmail.com>
2025-01-30 00:18:44 +0100

    Update bot_script.py

--- bot_script.py
+++ bot_script.py
@@ -11,13 +11,11 @@ from selenium.webdriver.chrome.service import Service
 TWITTER_USERNAME = os.getenv("TWITTER_USERNAME")
 TWITTER_PASSWORD = os.getenv("TWITTER_PASSWORD")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+CHROMEDRIVER_PATH = os.getenv('CHROMEDRIVER_PATH', '/app/.chromedriver/bin/chromedriver')
+GOOGLE_CHROME_BIN = os.getenv('GOOGLE_CHROME_SHIM', '/app/.apt/usr/bin/google-chrome')
 
 # Configurer l'API OpenAI
 openai.api_key = OPENAI_API_KEY
-
-# Configurer les chemins pour Heroku
-CHROMEDRIVER_PATH = os.getenv('CHROMEDRIVER_PATH', '/app/.chromedriver/bin/chromedriver')
-GOOGLE_CHROME_BIN = os.getenv('GOOGLE_CHROME_SHIM', '/app/.apt/usr/bin/google-chrome')
 
 # Configurer Selenium
 options = webdriver.ChromeOptions()
@@ -25,8 +23,10 @@ options.binary_location = GOOGLE_CHROME_BIN
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--remote-debugging-port=9222")
 
-# Initialiser le driver Selenium
+# Initialiser le driver avec les chemins fournis par Heroku
 driver = webdriver.Chrome(service=Service(CHROMEDRIVER_PATH), options=options)
 
 # Fonction pour générer un tweet avec hashtags
